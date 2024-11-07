ef785ae7b6de42061a07fe1596039b8f1c771d4d
g4mer9 <rileyfinkgamer9@gmail.com>
2024-11-07 15:47:51 -0800

    L16 done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 // todo
+//add button element to web page
+
+// Create a new button element
+const button = document.createElement("button");
+button.textContent = "Click Me";
+
+// Append the button to the body of the document
+document.body.appendChild(button);
+
+//on click, display alert('you clicked the button!')
+button.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
